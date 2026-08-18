3bd6221fef4ff932839bc5c54272996cd949d83e
gaurav developer <gauravgunjal857@gmail.com>
2026-08-18 17:45:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let total = document.getElementsByClassName("student")[0];
+
+let newadd = document.getElementById("addnew")[0];
+newadd.addEventListener("click",function (){
+    let information = document.createElement("table";
+        
+    )
+})
