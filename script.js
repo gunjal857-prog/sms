dc925aa393de1e44fb57a63282a2e54ee6a4e636
gaurav developer <gauravgunjal857@gmail.com>
2026-08-25 17:40:57 +0530

    Fix add employee redirection and show total employees
    Updated event listener for adding a new employee and corrected the URL for redirection.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let addnew = document.querySelector(".newstudent");
+// let total = document.getElementsByClassName("student")[0];
 
-addnew.addEventListener("click",function(){
-    window.location.href="add-employee.html"
-})
+// let newadd = document.getElementById("addnew")[0];
+// newadd.addEventListener("click",function (){
+//     let information = document.createElement("table");
+
+    
+// })
+function showtotalemployees(){
+
+        
+        let totalEmployee = document.querySelector("#shownum");
+        
+        let employees = JSON.parse(localStorage.getItem("employees")) || [];
+        
+        totalEmployee.innerText = employees.length;
+}
+showtotalemployees();
+
+let newemployee = document.querySelector("#addnew");
+newemployee.addEventListener("click", () => {
+        window.location.href="add-employe.html";
+
+
+
+});
